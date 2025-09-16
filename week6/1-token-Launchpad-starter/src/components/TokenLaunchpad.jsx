0b68a5cdd109b584cff353ba217b0d7cc263d134
amaan-malik1 <amikm2077@gmail.com>
2025-09-16 14:09:51 +0530

    Token creation done using DApp

--- week6/1-token-Launchpad-starter/src/components/TokenLaunchpad.jsx
+++ week6/1-token-Launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,4 +1,4 @@
-import { getMinimumBalanceForRentExemptMint } from '@solana/spl-token'
+import { ExtensionType, getMinimumBalanceForRentExemptMint, getMintLen, LENGTH_SIZE, TYPE_SIZE } from '@solana/spl-token'
 import { createInitializeMint2Instruction, MINT_SIZE, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { Connection, Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 import { useWallet } from "@solana/wallet-adapter-react";
@@ -8,31 +8,64 @@ function TokenLaunchpad() {
     const wallet = useWallet();
 
     async function createToken() {
+        const tokenName = document.getElementById("tokenName").value;
+        const symbol = document.getElementById("symbol").value;
+        const supply = document.getElementById("supply").value;
+        const imgUrl = document.getElementById("imgUrl").value;
+        console.log(tokenName, symbol, supply, imgUrl);
+
         const connection = new Connection(import.meta.env.VITE_SOLANA_RPC)
+
+        //generating keypair for the account that will hold the token
+        const mintKeypair = Keypair.generate();
+
+        //definiing the meatdata
+        const metadata = {
+            mint: mintKeypair.publicKey,
+            name: tokenName,
+            symbol: symbol,
+            uri: imgUrl,
+            addtionalmetadata: [],
+        };
+
+        //get the mintLength
+        // const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+        // const metaDataLen = TYPE_SIZE + LENGTH_SIZE + pac(metadata).length;
+
+
 
         //connect to solana devnet
         const lamports = await getMinimumBalanceForRentExemptMint(connection);
-        const keypair = Keypair.generate();
 
         //create transaction
         const transaction = new Transaction().add(
             SystemProgram.createAccount({
-                fromPubkey: keypair.publicKey,
-                newAccountPubkey: keypair.publicKey,
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintKeypair.publicKey,
                 space: MINT_SIZE,
                 lamports,
                 programId: TOKEN_PROGRAM_ID,
-            })
+            }),
+
+            createInitializeMint2Instruction(mintKeypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID),
         );
 
-        //create initialize mint instruction
-        createInitializeMint2Instruction(keypair.publicKey, 9, keypair.publicKey, keypair.publicKey, TOKEN_PROGRAM_ID);
 
-        //sign transaction with keypair
-        transaction.partialSign(keypair)
+        // TODO: what these cmds do?
+        transaction.feePayer = wallet.publicKey;
+        const recentBlockhash = await connection.getRecentBlockhash();
+        transaction.recentBlockhash = recentBlockhash.blockhash;
 
-        //modal for sign transaction
-        await wallet.sendTransaction(transaction);
+        //sign transaction with keypair and  here the keypair value is that we have generated in the createToken function
+        transaction.partialSign(mintKeypair);
+
+
+
+        //pop-up modal for sign transaction
+        await wallet.sendTransaction(transaction, connection);
+
+        ///printing the hash that has been generated 
+        // console.log(res);
 
     }
     return (
@@ -57,7 +90,7 @@ function TokenLaunchpad() {
                     id="symbol"
                     type="text"
                     required
-                    placeholder="Token Symbol"
+                    placeholder="Token Symbol: BTC, ETH, SOL, etc"
                     className="px-2 py-4 border border-gray-300 bg-gray-950 rounded" />
                 <input
                     id="supply"
@@ -71,7 +104,6 @@ function TokenLaunchpad() {
                     required
                     placeholder="Image `URL`"
                     className="px-2 py-4 border border-gray-300 bg-gray-950 rounded" />
-
                 <div className="flex justify-center items-center">
                     <button
                         onClick={createToken}
@@ -82,9 +114,7 @@ function TokenLaunchpad() {
                         Create Token
                     </button>
                 </div>
-
             </div>
-
         </div>
     )
 }
